a8ddf19d463552f0ab9a0d7f61c9f7097360b7d3
Alexander Thurn <at@froso.de>
2017-09-12 09:38:50 +0200

    working config file param

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 console.log('****** UFP OPTIMIZER started ******')
 console.log('****** USAGE:  node src/main.js [inputDir] [outputDir] [configFile] ******')
 
+const path = require('path')
 var myArgs = process.argv.slice(2);
 var inputDirName = myArgs && myArgs.length > 0 && myArgs[0] || 'example'
 var outputDirName = myArgs && myArgs.length > 1 && myArgs[1] || 'dist'
-var configFileName = myArgs && myArgs.length > 1 && myArgs[2] || './globals'
+var configFileName = myArgs && myArgs.length > 1 && path.join(process.cwd(), myArgs[2]) || path.resolve(__dirname, './globals.js')
 
-console.log('****** Input directory:', inputDirName, '  ******')
-console.log('****** Ouput directory:', outputDirName, '  ******')
-console.log('****** Config file:', configFileName, '  ******')
+console.log('****** USAGE:  node src/main.js ' + inputDirName + ' ' + outputDirName + ' ' + configFileName + ' ******')
+
+//console.log('****** Input directory:', inputDirName, '  ******')
+//console.log('****** Ouput directory:', outputDirName, '  ******')
+//console.log('****** Config file:', configFileName, '  ******')
 
 const app = require('./ufp-optimizer')
 const settings = require(configFileName)
