04012416a387ff5633f524d23df9aadc531ecb4d
Alexander Thurn <at@froso.de>
2017-09-26 11:38:24 +0200

    basic yargs non functional cli

--- bin/ufp-optimizer-cli.js
+++ bin/ufp-optimizer-cli.js
@@ -3,15 +3,49 @@
 const path = require('path')
 const app = require('./../src/UfpOptimizer')
 
+
+var argv = require('yargs').epilog('UFP Optimizer - Frontend Solutions 2017')
+    .usage('Use this tool to optimize your static assets (Javascript|CSS|PNG|JPG)')
+    .example('ufp-optimizer-cli optimize dist')
+    .example('ufp-optimizer-cli optimize dist distOptimized')
+    .example('ufp-optimizer-cli optimize dist distOptimized --env=development')
+    .example('ufp-optimizer-cli optimize dist distOptimized --env=production')
+    .example('ufp-optimizer-cli optimize dist distOptimized --config=myConfig.js')
+    .command(['optimize <inputDir> <targetDir>'], 'Optimizes all assets in the <inputDir> and writes them to <targetDir>', function () {
+            console.log('optimize command 1')
+        },
+        function () {
+            console.log('optimize command 2')
+        }
+    )
+    .command('config', 'Displays the config on console', function () {
+        console.log('config command')
+    })
+    .option('c', {
+        alias: 'config',
+        demandOption: false,
+        default: path.resolve(__dirname, '../src/Globals.js'),
+        describe: 'config file to use',
+        type: 'string'
+    }).option('e', {
+        alias: 'env',
+        demandOption: false,
+        choices: ['production', 'development', 'hardcore'],
+        default: 'production',
+        describe: 'Defines how strong it optimizes. production takes longer, hardcore takes veeery long. Also changes the cache duration for development purposes',
+        type: 'string'
+    }).demandCommand(1, 'You need at least one command before moving on')
+    .argv
+
+
 console.log('****** UFP OPTIMIZER 2.0 started ******')
-console.log('****** USAGE:  ufp-optimizer-cli [inputDir] [outputDir] [configFile] ******')
+//console.log('****** USAGE:  ufp-optimizer-cli ******')
 
-var myArgs = process.argv.slice(2)
-var inputDirName = (myArgs && myArgs.length > 0 && myArgs[0]) || 'examples/0/input'
-var outputDirName = (myArgs && myArgs.length > 1 && myArgs[1]) || 'dist'
-var configFileName = (myArgs && myArgs.length > 2 && path.join(process.cwd(), myArgs[2])) || path.resolve(__dirname, '../src/Globals.js')
+var inputDirName = (argv._ && argv._.length > 0 && argv._[0]) || 'examples/0/input'
+var outputDirName = (argv._ && argv._.length > 1 && argv._[1]) || 'dist'
+var configFileName = (argv.config && path.join(process.cwd(), argv.config)) || path.resolve(__dirname, '../src/Globals.js')
 
-console.log('****** USAGE:  ufp-optimizer-cli ' + inputDirName + ' ' + outputDirName + ' ' + configFileName + ' ******')
+//console.log('****** USAGE:  ufp-optimizer-cli ' + inputDirName + ' ' + outputDirName + ' ' + configFileName + ' ******')
 
 const settings = require(configFileName)
 
