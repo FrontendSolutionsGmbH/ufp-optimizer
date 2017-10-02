04d6e6374435929f5e97144d03e21efa28335258
Torsten Stelljes <ts@froso.de>
2017-10-02 15:19:55 +0200

    nice timeout intervals

--- tests/e2e-tests/test.js
+++ tests/e2e-tests/test.js
@@ -1,40 +1,46 @@
 const helper = require('./helper')
 
-describe('e2e-test', function() {
-  it('Should ignore null parameter', function(done) {
+describe('Fullpage Test', function() {
+  it('Should ignore null parameter', function (done) {
     const data = {
       inputDirName: 'examples/0/fullpage',
       outputDirName: 'dist-0',
       configFileName: ''
     }
     helper.build(data)
-    this.timeout(20000)
-    setTimeout(done, 10000)
+    this.timeout(10000)
+    setTimeout(done, 5000)
     expect(true).to.be.true
     console.log('helle')
   })
-  it('Should ignore null parameter', function(done) {
+})
+describe('Fast', function() {
+  it('Should ignore null parameter', function (done) {
     const data = {
       inputDirName: 'examples/1/fast',
       outputDirName: 'dist-1',
       configFileName: ''
     }
     helper.build(data)
-    this.timeout(20000)
-    setTimeout(done, 10000)
+    this.timeout(10000)
+    setTimeout(done, 5000)
     expect(true).to.be.true
   })
-  it('Should ignore null parameter', function(done) {
+})
+describe('Fast Test', function() {
+  it('Should ignore null parameter', function (done) {
     const data = {
       inputDirName: 'examples/1/fast',
       outputDirName: 'dist-2'
     }
     helper.build(data)
-    this.timeout(20000)
-    setTimeout(done, 10000)
+    this.timeout(10000)
+    setTimeout(done, 5000)
     expect(true).to.be.true
   })
-  it('Should ignore null parameter', function(done) {
+})
+describe('Same Dir Test', function() {
+  it('Should ignore null parameter', function (done) {
 
     var data = {
       inputDirName: 'examples/1/fast',
@@ -46,22 +52,26 @@ describe('e2e-test', function() {
       outputDirName: 'dist-3'
     }
     helper.build(data)
-    this.timeout(20000)
-    setTimeout(done, 10000)
+    this.timeout(10000)
+    setTimeout(done, 5000)
     expect(true).to.be.true
   })
-  it('Should ignore null parameter', function(done) {
+})
+describe('Broken Test', function() {
+  it('Should ignore null parameter', function (done) {
     const data = {
       inputDirName: 'examples/4/broken',
       outputDirName: 'dist-4',
       configFileName: ''
     }
     helper.build(data)
-    this.timeout(20000)
-    setTimeout(done, 10000)
+    this.timeout(10000)
+    setTimeout(done, 5000)
     expect(true).to.be.true
     console.log('helle')
   })
+})
+describe('Not Existing Test', function() {
   it('Should ignore null parameter', function(done) {
     const data = {
       inputDirName: 'examples/5/not-existing',
@@ -69,8 +79,8 @@ describe('e2e-test', function() {
       configFileName: ''
     }
     helper.build(data)
-    this.timeout(20000)
-    setTimeout(done, 10000)
+    this.timeout(5000)
+    setTimeout(done, 3000)
     expect(true).to.be.true
   })
 })
